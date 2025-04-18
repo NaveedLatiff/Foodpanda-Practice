a30871bb6c5d4a5472542e7b08cd083faed979ca
NaveedLatiff <naveedlatif526@gmail.com>
2025-04-18 14:34:01 +0500

    Removing FirebaseConfig

--- firebase.js
+++ firebase.js
@@ -3,13 +3,7 @@ import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, si
 import { getFirestore, doc, setDoc, getDoc, collection, addDoc, getDocs, query, orderBy, updateDoc, deleteDoc, where } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-firestore.js";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAF3UT9qG6QCP0S09d7hxKjTaspbhv8Xao",
-    authDomain: "foodpanda-88ab3.firebaseapp.com",
-    projectId: "foodpanda-88ab3",
-    storageBucket: "foodpanda-88ab3.firebasestorage.app",
-    messagingSenderId: "1087932517539",
-    appId: "1:1087932517539:web:5ed824bd3f62058aa74316",
-    measurementId: "G-PV2Y9FXPNZ"
+
 };
 
 const app = initializeApp(firebaseConfig);
